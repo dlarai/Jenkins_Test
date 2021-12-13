1241eb7ae4241638e60bb83000b1e998c9cc8ee0
Dilarai <dilarabolen@gmail.com>
2021-12-13 23:11:10 +0100

    Hey

---
+++ BuildMavenProject/src/test/java/buildMavenProject/TestMaven.java
@@ -0,0 +1,14 @@
+package buildMavenProject;
+
+import org.testng.annotations.Test;
+
+public class TestMaven {
+
+	 @Test
+	 public void testMaven() {
+		 
+		 System.out.println("Hi Maven..");
+		 
+	
+}
+}
